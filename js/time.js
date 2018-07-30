03d75387c7f8beefc2819a7aff5b69a233876e66
ytx21cn <>
2018-07-30 23:59:46 +0800

    css update and date display update

--- js/time.js
+++ js/time.js
@@ -1,13 +1,18 @@
 "use strict";
+const date = document.getElementById("date");
 const time = document.getElementById("time");
 
 function getCurrentTime () {
 	
 	let today = new Date();
+	let YYYY = today.getFullYear();
+	let MM = checkNum(today.getMonth() + 1);
+	let DD = checkNum(today.getDate());
 	let hh = checkNum(today.getHours());
 	let mm = checkNum(today.getMinutes());
 	let ss = checkNum(today.getSeconds());
 	
+	date.innerHTML = YYYY + "-" + MM + "-" + DD;
 	time.innerHTML = hh + ":" + mm + ":" + ss;
 	
 	let t = setTimeout(getCurrentTime, 100); 
